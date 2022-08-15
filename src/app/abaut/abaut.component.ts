531d3ca55c352c64e76c494873a50b99642c2cd4
Javier <javierscarione@gmail.com>
2022-08-15 19:03:51 -0300

    Reactive Forms

--- src/app/abaut/abaut.component.ts
+++ src/app/abaut/abaut.component.ts
@@ -15,7 +15,7 @@ export class AbautComponent implements OnInit {
 
   ngOnInit() {
     this.leaderService.getLeaders()
-    .then((leaders) => this.leaders = leaders)
+    .subscribe((leaders) => this.leaders = leaders)
   }
 
 }
